2a57e223f4d4425e1b858d631c5fe301b2769d4f
tomandes <tomandes@tuta.io>
2026-05-21 20:05:01 +0200

    changed size

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/l
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-camera.position.z = 5; 
+camera.position.z = 20; 
 
 // Globální proměnné pro ovládání
 let controls;
